ef44ebe651874c74280e52e666fb907cd148e6fc
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-05 16:59:26 -0700

    tasks 02

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,58 +1,48 @@
 #!/usr/bin/python3
-""" generates a .tgz archive
-"""
-from fabric.api import local, env, put, run
-from datetime import datetime
-from os import path
-from fabric.decorators import runs_once
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-
-env.hosts = ['52.90.14.193', '54.90.50.39']
-
-# Set the username
-env.user = "ubuntu"
-
-# Set private key path
-env.key_filename = "~/.ssh/id_rsa"
-
-
-@runs_once
-def do_pack():
-    """ generates a .tgz archive from the contents of
-    the web_static folder of your AirBnB Clone repo
-    """
-
-    local("mkdir -p versions")
-    dformat = "%Y%m%d%H%M%S"
-    archive_path = "versions/web_static_{}.tgz".format(
-            datetime.strftime(datetime.now(), dformat))
-    result = local("tar -cvzf {} web_static".format(archive_path))
-    if result.failed:
-        return None
-    return archive_path
+env.hosts = ["52.90.14.193", "54.90.50.39"]
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers"""
-
-    try:
-        if not path.exists(archive_path):
-            return False
-
-        dir_path = "/data/web_static/releases/"
-        filename = path.basename(archive_path)
-        file_no_ext, ext = path.splitext(filename)
-        put(archive_path, "/tmp/{}".format(filename))
-        run("rm -rf {}{}".format(dir_path, file_no_ext))
-        run("mkdir -p {}{}".format(dir_path, file_without_ext))
-        run("tar -xzf /tmp/{} -C {}{}".format(filename, dir_path, file_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dir_path, file_no_ext))
-        run("rm -rf {}{}/web_static".format(dir_path, file_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(
-            dir_path, file_no_ext))
-        print("New version deployed!")
-        return True
-    except Exception:
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
