e31f9ddd73bf934f6fb5f193eddc3940c829bd22
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-21 12:34:19 -0700

    tasks 01

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,9 +10,10 @@ def Home():
     return "Hello HBNB!"
 
 
-app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    retutn "HBNB"
+
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
