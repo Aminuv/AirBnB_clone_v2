f4f993e02fe2c70968a4de6b4d4b0829b419e629
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-21 12:20:54 -0700

    tasks 01

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/, strict_slashes=False')
+@app.route("/", strict_slashes=False)
 def Home():
     return "Hello HBNB!"
 
-if __name__ == "__amin__":
-    app.run(host="0.0.0.0', post=5000)
+if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000)
