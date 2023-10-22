fcfb87081ede83f00543cb28049b1cc54c0ac7b5
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-22 12:22:19 -0700

    tasks 06

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Module to start a flask web app """
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Home():
+    """ display Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ display HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_c(text):
+    """ C is called """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ display doc paythone """
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def disp_num(n):
+    """ desplay the doc number """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def disp_num_temp(n):
+     """ display a HTML"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def disp_odd_or_even(n):
+    """display HTML Page with Number """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
