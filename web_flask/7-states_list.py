b4e97006af86f4b57bcc0f7cafaa78196fe58bf2
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-10-22 13:26:59 -0700

    tasks 07

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module to start a flask web app """
+from flask import Flask
+from flask import render_template
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def disp_state():
+    """ displayn an HTML """
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def tear_down(excep):
+    """Call Storage.close method"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
