0621b927c28cf2f84cf621cfcaceb1f19792df98
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-23 01:06:11 +0000

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,21 +2,74 @@
 """ Module to start a flask web app """
 from flask import Flask
 from flask import render_template
-from models.state import State
 app = Flask(__name__)
 
-@app.route("/cities_by_states", strict_slashes=False)
-def disp_cities():
-    """ display an HTML Page of cities"""
-    states = storage.all(State).values()
-    return render_template("8-cities_by_states.html", states=states)
+
+@app.route("/", strict_slashes=False)
+def Home():
+    """ display Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ display HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_c(text):
+    """ C is called """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ display doc paythone """
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def disp_num(n):
+    """ desplay the doc number """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def disp_num_temp(n):
+    return render_template('5-number.html', number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def disp_even_odd(n):
+    """display HTML Page with Number """
+    if n % 2 == 0:
+        p = 'even'
+    else:
+        p = 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, parity=p)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ statut list it """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tear_down(excep):
-    """ close function"""
+def close(error):
+    """ Close """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_states():
+    """ city by states """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
