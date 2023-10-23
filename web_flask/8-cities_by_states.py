a7a98048186c4a7b21f75ab60596610028a64427
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-23 01:55:18 +0000

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -54,11 +54,5 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def close(error):
-    """ Close """
-    storage.close()
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
