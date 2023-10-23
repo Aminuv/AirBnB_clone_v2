a3b1330a5f9e5f2b1443010024370090a0803447
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-23 00:10:23 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
-from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models import storage_type
 
 
 class State(BaseModel, Base):
-    """ State class """
-
+    """ Represents the State class """
     __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade="all,delete", backref="state")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """ getter for the cities attribute """
+            from models import storage
+
+            all_cities = storage.all(City)
+            return [city for city in all_cities.values()
+                    if city.state_id == self.id]
