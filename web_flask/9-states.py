8b7e4eafd02be78a55b03025a2d9efbbc6243f9a
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-23 02:16:12 +0000

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""Start flask web application must be listening
-on 0.0.0.0, port 5000
-"""
+""" Module to start a flask web app """
 
 from flask import Flask, render_template
 from models.state import State
 from models import storage
-
-
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_state():
-    """return an HTML Page that displays all states"""
-    states = sorted(storage.all(State).values(), key=lambda x: x.name)
-    return render_template("7-states_list.html", states=states)
+@app.route("/states/<state_id>", strict_slashes=False)
+@app.route("/states", strict_slashes=False)
+def display_states(state_id=None):
+    """ display an 'HTML' Page  of states and cities"""
+    states = storage.all(State)
+    if state_id is not None:
+        state_id = f'State.{state_id}'
+    return render_template("9-states.html", states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
-def teardown(excep):
-    """Call Storage.close method"""
+def teardown(execpt):
+    """ storage close """
     storage.close()
 
 
