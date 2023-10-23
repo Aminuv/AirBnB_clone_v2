8e96c43ef815af22a22683ee70fb59b06c3dee64
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-23 02:14:07 +0000

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ Module to start a flask web app """
-from flask import Flask
-from flask import render_template
-from models import storage
+
+from flask import Flask, render_template
 from models.state import State
+from models import storage
 app = Flask(__name__)
+
 
 @app.route("/cities_by_states", strict_slashes=False)
 def display_cities():
-    """display an 'HTML' Page"""
+    """ Display an HTML Page of citie """
     states = storage.all(State).values()
     return render_template("8-cities_by_states.html", states=states)
 
@@ -19,5 +20,5 @@ def teardown(excep):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
