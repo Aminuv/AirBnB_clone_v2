bfa50c86cbd494d782523bdab8a84b1918d55a50
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-26 00:46:04 +0000

    Update city

--- models/city.py
+++ models/city.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-""" ciity Module for HBNB project """
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """The city class, contains state ID and name"""
+    """Represents the city class
+    Class Attribute:
+        __tablename__: the mapping table
+        name: sqlalchemy String Column : the city name
+        state_id: sqlalchemy String Column : the state identifier (FK)
+    """
+    __tablename__ = 'cities'
 
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    if storage_type != 'db':
+        name = ''
+        state_id = ''
+    else:
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey("states.id"),
+                          nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete')
