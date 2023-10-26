aee69efe47ec5c8859bacae9ba49dbdfe47a5b31
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-26 00:44:44 +0000

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,15 @@
-#!/usr/bin/pyithon3
-""" State Module for HBNB project """
-
+#!/usr/bin/python3
+""" Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from models import storage_type
 
 
 class Amenity(BaseModel, Base):
+    """Represents Amenity Class"""
     __tablename__ = 'amenities'
 
-    name = ""
+    if storage_type != 'db':
+        name = ""
+    else:
+        name = Column(String(128), nullable=False)
