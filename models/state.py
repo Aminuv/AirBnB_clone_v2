d9e9aa85504294cbd42fb5c04fce83e48b7c067f
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-26 00:33:16 +0000

    Update state 07

--- models/state.py
+++ models/state.py
@@ -8,7 +8,11 @@ from models import storage_type
 
 
 class State(BaseModel, Base):
-    """ Represents the State class """
+    """ Represents the State class
+    Class Attributes:
+        name: sqlalchemy String Column: the state name
+        cities: represent a relationship with the class City
+    """
     __tablename__ = 'states'
 
     if storage_type == 'db':
@@ -20,7 +24,12 @@ class State(BaseModel, Base):
 
         @property
         def cities(self):
-            """ getter for the cities attribute """
+            """ Getter for the cities attribute
+            * returns the list of City instances with state_id equals
+            to the current State.id
+            * It will be the FileStorage relationship between State and
+            City
+            """
             from models import storage
 
             all_cities = storage.all(City)
