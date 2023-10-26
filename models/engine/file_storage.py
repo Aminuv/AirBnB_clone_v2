252783bac5ec54ca255d6237cf93059344c51a5f
mohamed elmarbouh <125465547+Aminuv@users.noreply.github.com>
2023-10-26 00:29:52 +0000

    Update file_storage 07

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+""" This module defines a class to manage file storage for hbnb clone """
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """ This class manages storage of hbnb models in JSON format """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """ Returns a dictionary of models currently in storage """
         if cls is None:
             return FileStorage.__objects
         class_name = cls.__name__
@@ -18,11 +18,11 @@ class FileStorage:
                 if key.split('.')[0] == class_name}
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """ Adds new object to storage dictionary """
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """ Saves storage dictionary to file """
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -31,7 +31,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """ Loads storage dictionary from file """
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -55,7 +55,7 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Deletes obj from __objects if exists"""
+        """ Deletes obj from __objects if exists """
         if obj is None:
             return
         del_key = f"{obj.to_dict()['__class__']}.{obj.id}"
